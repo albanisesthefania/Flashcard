53014536f24709145feadeb59c2f0b62fed95c7e
albanisesthefania <167584246+albanisesthefania@users.noreply.github.com>
2024-10-24 11:37:31 -0300

    Add files via upload

---
+++ pf/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Finlândia?',
+    'Helsinque'
+)
+
+criaCartao(
+    'Geografia',
+    'Em qual continente se encontra a Guatemala?',
+    'América Central'
+)
+
+criaCartao(
+    'Astronomia',
+    'O que é uma função?',
+    'Qual planeta tem anéis?'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o planeta que está mais cerca do Sol?',
+    'Mercúrio'
+)
